f6356a4acd36f1b784740eaf0b990bca9d10674a
yaroslav <bus.yaroslav@gmail.com>
2017-10-20 18:55:52 +0300

    projectCreated

---
+++ RetranslationServers/Test/Head.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RetranslationServers
+{
+    class Head
+    {
+        static List<Pipe> pipeList;
+        public static void Main(string[] args)
+        {
+            pipeList = new List<Pipe>();
+            pipeList.Add(new Pipe("192.168.1.173", "192.168.1.240", 5000, 5001));
+            pipeList.Add(new Pipe("192.168.1.173", "192.168.1.240", 5001, 5000));
+            foreach (Pipe currentPipe in pipeList)
+            {
+                Thread pipeThread = new Thread(currentPipe.Listen);
+                pipeThread.Start();
+            }
+
+        }
+    }
+}
---
+++ RetranslationServers/Test/Pipe.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace RetranslationServers
+{
+    class Pipe
+    {
+        TcpListener serverIn;
+        TcpClient clientOut;
+        int portIn;
+        int portOut;
+        IPAddress localAdress1;
+        string localAdress2;
+
+        public Pipe(string local1, string local2, int port1, int port2)
+        {
+
+            localAdress1 = IPAddress.Parse(local1);
+            localAdress2 = local2;
+            portIn = port1;
+            portOut = port2;
+        }
+
+        public void Listen()
+        {
+            serverIn = new TcpListener(localAdress1, portIn);
+            clientOut = new TcpClient(localAdress2, portOut);
+            serverIn.Start();
+
+            while (true)
+            {
+                TcpClient clientIn = serverIn.AcceptTcpClient();
+                string nameOfClient = ((IPEndPoint)clientIn.Client.LocalEndPoint)+"";
+                clientIn.ReceiveBufferSize = 1024;
+                NetworkStream streamIn = clientIn.GetStream();
+                NetworkStream streamOut = clientOut.GetStream();
+                byte[] bytes = new byte[clientIn.ReceiveBufferSize];
+                string data;
+                int i;
+                string welcomeMsg = "Write 'exit' to exit chat\n";
+                byte[] msg = Encoding.ASCII.GetBytes(welcomeMsg);
+                streamIn.Write(msg, 0, msg.Length);
+                while ((i = streamIn.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    data = Encoding.ASCII.GetString(bytes, 0, i);
+                    string sendingData = nameOfClient + ": " + data;
+                    msg = Encoding.ASCII.GetBytes(sendingData);
+                    streamOut.Write(msg, 0, msg.Length);
+                    Console.WriteLine(data);
+                    if (data.Equals("exit\n"))
+                    {
+                        streamIn.Close();
+                        streamOut.Close();
+                        Exit();
+                        return;
+                    }
+                }
+                streamIn.Close();
+                streamOut.Close();
+            }
+        }
+
+        public void Exit()
+        {
+            serverIn.Stop();
+        }
+    }
+}
